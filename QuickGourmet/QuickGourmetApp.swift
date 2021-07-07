72fdfdfe002ceda205649c99d4bdd203db8b538a
佐々木　謙 <nkekisasa222@gmail.com>
2021-07-08 07:25:16 +0900

    Initial Commit

---
+++ QuickGourmet/QuickGourmetApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickGourmetApp.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct QuickGourmetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
