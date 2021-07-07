ca02fdb71f843f202b70734a35bea698d2eb2ce5
佐々木　謙 <nkekisasa222@gmail.com>
2021-07-08 08:34:43 +0900

    Create InputVIew
    最低限のUIで入力フォームと検索ボタンを作成

---
+++ QuickGourmet/InputView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/07/08.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    @State var keywordText = ""
+    @State var genreText = ""
+        
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("必須")) {
+                    TextField("駅、エリアを入力", text: $keywordText)
+                }
+                
+                Section(header: Text("必須")) {
+                    TextField("料理ジャンルを入力", text: $genreText)
+                }
+                
+                HStack {
+                    Spacer()
+                    Button("検索") {
+                        print("検索開始")
+                    }
+                    Spacer()
+                }
+            }
+            .navigationTitle("さくっとグルメ")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView()
+    }
+}
