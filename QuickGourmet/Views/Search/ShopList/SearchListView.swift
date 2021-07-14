819657da11906764121ab8f29957077b10850a54
佐々木　謙 <nkekisasa222@gmail.com>
2021-07-14 11:07:08 +0900

    Create SearchListView.swift
    検索結果を表示するListVewを作成。
    現状Mockで対応。

---
+++ QuickGourmet/Views/Search/ShopList/SearchListView.swift
@@ -0,0 +1,33 @@
+//
+//  SearchListView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/07/13.
+//
+
+import SwiftUI
+
+struct SearchListView: View {
+    
+    let shopData: [Shop] = mockShopesData
+    
+    // ObservableObjectに準拠したクラスを監視
+    @ObservedObject var shopSearchFetcher = ShopSearchFetcher()
+    
+    var body: some View {
+        NavigationView {
+            List((shopData)) { shop in
+                NavigationLink(destination: ShopDetailView()) {
+                    ShopRowView(shopData: shopData[0])
+                }
+            }
+            .navigationTitle(Text("検索結果一覧"))
+        }
+    }
+}
+
+struct SearchListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchListView()
+    }
+}
