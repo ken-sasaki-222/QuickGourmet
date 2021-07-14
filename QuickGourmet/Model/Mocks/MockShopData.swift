f23ece65b492159dcbd9cbd7f4f7dc7b287b4a63
佐々木　謙 <nkekisasa222@gmail.com>
2021-07-14 10:55:00 +0900

    Create MockShopData.swift
    Mockデータを作成。
    これで、API通信後の任意の状況を作り出せる。また、Interfaceを定義することで開発効率が高まる。

---
+++ QuickGourmet/Model/Mocks/MockShopData.swift
@@ -0,0 +1,74 @@
+//
+//  MockShopData.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/07/14.
+//
+
+import Foundation
+
+// Search API
+// https://webservice.recruit.co.jp/hotpepper/gourmet/v1/
+
+let mockShopesData: [Shop]
+    = [Shop(id: "J001265507",
+            name: "焼肉少年團 渋谷店",
+            mobileAccess: "渋谷駅ﾊﾁ公口から徒歩5分",
+            open: "月～日、祝日、祝前日: 17:00～23:00 （料理L.O. 22:30 ドリンクL.O. 22:30）",
+            lat: Int(35.6610492907),
+            lng: Int(139.6968179396),
+            genre: Shop.Genre(catche: "渋谷 -焼肉食べ放題- 黒毛和牛食べ放題95品",
+                              code: "G008",
+                              genreName: "焼肉・ホルモン"),
+            photo: Shop.Photo(mobile: Shop.Photo.Mobile(l: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_168.jpg",
+                                                        s: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_100.jpg")),
+            urls: Shop.URLS(pc: "https://www.hotpepper.jp/strJ001265507/?vos=nhppalsa000016")),
+       Shop(id: "J001265507",
+            name: "焼肉少年團 渋谷店",
+            mobileAccess: "渋谷駅ﾊﾁ公口から徒歩5分",
+            open: "月～日、祝日、祝前日: 17:00～23:00 （料理L.O. 22:30 ドリンクL.O. 22:30）",
+            lat: Int(35.6610492907),
+            lng: Int(139.6968179396),
+            genre: Shop.Genre(catche: "渋谷 -焼肉食べ放題- 黒毛和牛食べ放題95品",
+                              code: "G008",
+                              genreName: "焼肉・ホルモン"),
+            photo: Shop.Photo(mobile: Shop.Photo.Mobile(l: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_168.jpg",
+                                                        s: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_100.jpg")),
+            urls: Shop.URLS(pc: "https://www.hotpepper.jp/strJ001265507/?vos=nhppalsa000016")),
+       Shop(id: "J001265507",
+            name: "焼肉少年團 渋谷店",
+            mobileAccess: "渋谷駅ﾊﾁ公口から徒歩5分",
+            open: "月～日、祝日、祝前日: 17:00～23:00 （料理L.O. 22:30 ドリンクL.O. 22:30）",
+            lat: Int(35.6610492907),
+            lng: Int(139.6968179396),
+            genre: Shop.Genre(catche: "渋谷 -焼肉食べ放題- 黒毛和牛食べ放題95品",
+                              code: "G008",
+                              genreName: "焼肉・ホルモン"),
+            photo: Shop.Photo(mobile: Shop.Photo.Mobile(l: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_168.jpg",
+                                                        s: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_100.jpg")),
+            urls: Shop.URLS(pc: "https://www.hotpepper.jp/strJ001265507/?vos=nhppalsa000016")),
+       Shop(id: "J001265507",
+            name: "焼肉少年團 渋谷店",
+            mobileAccess: "渋谷駅ﾊﾁ公口から徒歩5分",
+            open: "月～日、祝日、祝前日: 17:00～23:00 （料理L.O. 22:30 ドリンクL.O. 22:30）",
+            lat: Int(35.6610492907),
+            lng: Int(139.6968179396),
+            genre: Shop.Genre(catche: "渋谷 -焼肉食べ放題- 黒毛和牛食べ放題95品",
+                              code: "G008",
+                              genreName: "焼肉・ホルモン"),
+            photo: Shop.Photo(mobile: Shop.Photo.Mobile(l: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_168.jpg",
+                                                        s: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_100.jpg")),
+            urls: Shop.URLS(pc: "https://www.hotpepper.jp/strJ001265507/?vos=nhppalsa000016")),
+       Shop(id: "J001265507",
+            name: "焼肉少年團 渋谷店",
+            mobileAccess: "渋谷駅ﾊﾁ公口から徒歩5分",
+            open: "月～日、祝日、祝前日: 17:00～23:00 （料理L.O. 22:30 ドリンクL.O. 22:30）",
+            lat: Int(35.6610492907),
+            lng: Int(139.6968179396),
+            genre: Shop.Genre(catche: "渋谷 -焼肉食べ放題- 黒毛和牛食べ放題95品",
+                              code: "G008",
+                              genreName: "焼肉・ホルモン"),
+            photo: Shop.Photo(mobile: Shop.Photo.Mobile(l: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_168.jpg",
+                                                        s: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_100.jpg")),
+            urls: Shop.URLS(pc: "https://www.hotpepper.jp/strJ001265507/?vos=nhppalsa000016")),
+    ]
