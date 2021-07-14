1bb62b23522300fb2acc7e6b604c40e6d28845e5
佐々木　謙 <nkekisasa222@gmail.com>
2021-07-14 11:08:49 +0900

    Create ShopDetailView.swift
    検索結果のListからタップで遷移する詳細画面。

---
+++ QuickGourmet/Views/Search/ShopDetail/ShopDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ShopDetailView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/07/13.
+//
+
+import SwiftUI
+
+struct ShopDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShopDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopDetailView()
+    }
+}
