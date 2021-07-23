9538ba12ab3abad53287e36925f5d4476d5d1cfd
佐々木　謙 <nkekisasa222@gmail.com>
2021-07-23 16:03:16 +0900

    add: お店詳細画面のSafariへの遷移ボタンを扱うファイルを作成

---
+++ QuickGourmet/Views/Search/ShopDetail/ShopDetailButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  ShopDetailButtonView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/07/23.
+//
+
+import SwiftUI
+
+struct ShopDetailButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShopDetailButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopDetailButtonView()
+    }
+}
