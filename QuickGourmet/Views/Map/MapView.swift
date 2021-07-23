2a72802e393067a74951100058f921e287d0f86d
佐々木　謙 <nkekisasa222@gmail.com>
2021-07-23 17:25:16 +0900

    add: 詳細画面に表示するMapViewを作成

---
+++ QuickGourmet/Views/Map/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/07/23.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
