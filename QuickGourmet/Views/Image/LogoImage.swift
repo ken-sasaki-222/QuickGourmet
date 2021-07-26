fc82e2f0c8e15824af348994d5513fb07a702b9e
佐々木　謙 <nkekisasa222@gmail.com>
2021-07-27 00:39:30 +0900

    feat: 店舗詳細Viewで使うロゴイメージを扱うファイルを作成

---
+++ QuickGourmet/Views/Image/LogoImage.swift
@@ -0,0 +1,48 @@
+//
+//  LogoImage.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/07/27.
+//
+
+import SwiftUI
+
+struct LogoImage: View {
+    @ObservedObject private var imageDownloader = ImageDownloader()
+    let urlString: String
+    
+    init(urlString: String) {
+        self.urlString = urlString
+        self.imageDownloader.downloadImage(url: self.urlString)
+    }
+    
+    var body: some View {
+        if let imageData = self.imageDownloader.downloadData {
+            guard let img = UIImage(data: imageData) else {
+                return VStack {
+                    Image(uiImage: UIImage(systemName: "icloud.and.arrow.down")!)
+                        .resizable()
+                        .frame(width: 35, height: 32, alignment: .center)
+                }
+            }
+            return VStack {
+                Image(uiImage: img)
+                    .resizable()
+                    .frame(width: 100, height: 100, alignment: .center)
+            }
+        } else {
+            return VStack {
+                Image(uiImage: UIImage(systemName: "icloud.and.arrow.down")!)
+                    .resizable()
+                    .frame(width: 35, height: 32, alignment: .center)
+            }
+        }
+    }
+}
+
+
+struct LogoImage_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoImage(urlString: "https://imgfp.hotp.jp/IMGH/54/14/P037425414/P037425414_168.jpg")
+    }
+}
