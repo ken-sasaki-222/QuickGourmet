004bb49da83b590b70cde2eaafdda8f7daea46e0
佐々木　謙 <nkekisasa222@gmail.com>
2021-07-29 11:52:22 +0900

    feat: メニューページ各Formにたしてアクションを追加
    機能的コードはMenuVMで管理。
    お問い合わせ機能はMailViewを新たに作成してバインディング。

---
+++ QuickGourmet/ViewModel/Menu/MenuViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  MenuViewModel.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/07/27.
+//
+
+import Foundation
+import SwiftUI
+import MessageUI
+
+class MenuViewModel {
+    enum DevelopperUrlType: String {
+        case twitter = "https://twitter.com/ken_sasaki2"
+        case gitHub = "https://github.com/ken-sasaki-222"
+        case qiita = "https://qiita.com/nkekisasa222"
+    }
+    
+    func shareOnTwitter() {
+        let message = "飲食店をクイック検索！ \n 『食いっくグルメ』をダウンロードしよう。"
+        
+        // URLクエリ内で使用できる文字列に変換
+        guard let encodedMessage = message.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return
+        }
+        
+        guard let tweetURL = URL(string: "https://twitter.com/intent/tweet?text=\(encodedMessage)") else {
+            return
+        }
+        
+        UIApplication.shared.open(tweetURL, options: [:], completionHandler: nil)
+    }
+    
+    func goToDeveloperSNSPage(snsString: String) {
+        var snsUrlString = String()
+        
+        switch snsString {
+        case "twitter":
+            snsUrlString = DevelopperUrlType.twitter.rawValue
+        case "gitHub":
+            snsUrlString = DevelopperUrlType.gitHub.rawValue
+        case "qiita":
+            snsUrlString = DevelopperUrlType.qiita.rawValue
+        default:
+            break
+        }
+       
+        guard let snsUrl = URL(string: snsUrlString) else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(snsUrl) {
+            UIApplication.shared.open(snsUrl, options: [:], completionHandler: nil)
+        }
+    }
+}
--- QuickGourmet/Views/Menu/MenuView.swift
+++ QuickGourmet/Views/Menu/MenuView.swift
@@ -6,83 +6,91 @@
 //
 
 import SwiftUI
+import StoreKit
+import MessageUI
 
 struct MenuView: View {
+    @State private var isShowMailView = false
+    var menuVM = MenuViewModel()
+    
     var body: some View {
-        Form {
-            Section(header: Text("このアプリについて")) {
-                HStack {
-                    Image("mail_icon")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .padding(10)
-                    Button ("ご意見・ご要望") {
-                        print("お問い合わせ")
+        NavigationView {
+            Form {
+                Section(header: Text("このアプリについて")) {
+                    HStack {
+                        Image("mail_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(10)
+                        Button("ご意見・ご要望") {
+                            isShowMailView = true
+                        }
+                        .disabled(!MFMailComposeViewController.canSendMail())
+                        .sheet(isPresented: $isShowMailView) {
+                            MailView(isShow: $isShowMailView)
+                        }
                     }
-                    .foregroundColor(.black)
+                    HStack {
+                        Image("review_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(10)
+                        Button("レビュー") {
+                            if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                                SKStoreReviewController.requestReview(in: scene)
+                            }
+                        }
+                    }
+                    HStack {
+                        Image("share_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(10)
+                        Button("シェア") {
+                            menuVM.shareOnTwitter()
+                        }
+                    }
                 }
-                
-                HStack {
-                    Image("share_icon")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .padding(10)
-                    Button("シェア") {
-                        
+                Section(header: Text("開発者")) {
+                    HStack {
+                        Image("twitter_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(10)
+                        Button("Twitter") {
+                            menuVM.goToDeveloperSNSPage(snsString: "twitter")
+                        }
                     }
-                    .foregroundColor(.black)
+                    HStack {
+                        Image("github_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(10)
+                        Button("GitHub") {
+                            menuVM.goToDeveloperSNSPage(snsString: "gitHub")
+                        }
+                    }
+                    HStack {
+                        Image("qiita_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(10)
+                        Button("Qiita") {
+                            menuVM.goToDeveloperSNSPage(snsString: "qiita")
+                        }
+                    }
+                }
+                Section(header: Text("バージョン")) {
+                    HStack {
+                        Image("version_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(10)
+                        Text("バージョン 1.0")
+                    }
                 }
             }
-            
-            Section(header: Text("開発者")) {
-                
-                HStack {
-                    Image("twitter_icon")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .padding(10)
-                    Button("Twitter") {
-                        
-                    }
-                    .foregroundColor(.black)
-                }
-                
-                HStack {
-                    Image("github_icon")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .padding(10)
-                    Button("GitHub") {
-                        
-                    }
-                    .foregroundColor(.black)
-                    
-                }
-                
-                HStack {
-                    Image("qiita_icon")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .padding(10)
-                    Button("Qiita") {
-                        
-                    }
-                    .foregroundColor(.black)
-                }
-            }
-            
-            Section(header: Text("バージョン")) {
-                HStack {
-                    Image("version_icon")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .padding(10)
-                    Button("バージョン  1.0") {
-                        
-                    }
-                    .foregroundColor(.black)
-                }
-            }
+            .navigationTitle("メニュー")
         }
     }
 }
