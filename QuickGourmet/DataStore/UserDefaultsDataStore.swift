d0ad9e3c107c1fe6d290fbc6ded8fc21f8010135
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-03 20:44:08 +0900

    add: UserDefaultsを管理するファイルを作成

---
+++ QuickGourmet/DataStore/UserDefaultsDataStore.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsDataStore.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/03.
+//
+
+import Foundation
+
+class UserDefaultsDataStore {
+    
+    var defalts: UserDefaults {
+        return UserDefaults.standard
+    }
+    
+    
+}
