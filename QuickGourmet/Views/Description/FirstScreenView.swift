70b94c786308dd827f29035b21e6513158bb0f07
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-11 09:44:15 +0900

    add: 最初の画面を扱うファイルの作成

---
+++ QuickGourmet/Views/Description/FirstScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstScreenView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/11.
+//
+
+import SwiftUI
+
+struct FirstScreenView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct FirstScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstScreenView()
+    }
+}
