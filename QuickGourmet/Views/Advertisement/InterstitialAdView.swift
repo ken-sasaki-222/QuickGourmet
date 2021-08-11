895d0f05dc243a4dd81cc18bf66ec669344fe3b5
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-11 21:58:51 +0900

    add: インタースティシャル広告を管理するファイルを作成

---
+++ QuickGourmet/Views/Advertisement/InterstitialAdView.swift
@@ -0,0 +1,20 @@
+//
+//  InterstitialAdView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/11.
+//
+
+import SwiftUI
+
+struct InterstitialAdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InterstitialAdView_Previews: PreviewProvider {
+    static var previews: some View {
+        InterstitialAdView()
+    }
+}
