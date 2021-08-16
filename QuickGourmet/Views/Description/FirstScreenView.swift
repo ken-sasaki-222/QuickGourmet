3ee78761baec59cb72d422d3e4a2d86cd21cefe4
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-17 00:18:49 +0900

    feat: 暫定の初期起動画面を追加

--- QuickGourmet/Views/Description/FirstScreenView.swift
+++ QuickGourmet/Views/Description/FirstScreenView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct FirstScreenView: View {
+    @State private var isTapActived = false
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            ZStack {
+                Image("first_view_background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                Color.white.opacity(0.5)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("食いっくグルメはユーザーの現在地から\n徒歩圏内の飲食店情報を提供します。")
+                        .fontWeight(.medium)
+                        .font(.title3)
+                        .lineLimit(3)
+                        .offset(x: 0, y: -200)
+                    NavigationLink(destination: QuickSearchView(), isActive: $isTapActived) {
+                        Button(action: {
+                            isTapActived.toggle()
+                        }) {
+                            Text("食いっくグルメをはじめる")
+                                .foregroundColor(Color.white)
+                                .fontWeight(.medium)
+                                .font(.title3)
+                                .padding(.vertical, 20)
+                                .padding(.horizontal, 60)
+                                .background(Color.green)
+                                .cornerRadius(100)
+                                .lineLimit(1)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("食いっくグルメ")
+        }
     }
 }
 
