07cdeba053f5899fcb9e7c4d74cbb184daa65776
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-18 17:37:21 +0900

    add: ユーザー情報の取得保存を行うファイルを作成

---
+++ QuickGourmet/Repository/UserRepository.swift
@@ -0,0 +1,8 @@
+//
+//  UserRepository.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/18.
+//
+
+import Foundation
---
+++ QuickGourmet/Repository/UserRepositoryInterface.swift
@@ -0,0 +1,8 @@
+//
+//  UserRepositoryInterface.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/18.
+//
+
+import Foundation
