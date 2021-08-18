1a6aaf8e226d942e33485f6242b01252b3c9488c
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-18 11:17:37 +0900

    add: ログインボタンを管理するファイルを作成

---
+++ QuickGourmet/Views/Button/LoginButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginButton.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/18.
+//
+
+import SwiftUI
+
+struct LoginButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginButton_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginButtonView()
+    }
+}
