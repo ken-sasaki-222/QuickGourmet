c0200226e1b8857c2355fdb1dca1bcc47fd315d3
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-21 21:12:57 +0900

    add: お気に入り一覧を表示するファイルの作成

---
+++ QuickGourmet/Views/Search/ShopList/Favorite/FavoriteListView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteListView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/21.
+//
+
+import SwiftUI
+
+struct FavoriteListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteListView()
+    }
+}
