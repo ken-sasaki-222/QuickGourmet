df7666d267b3b4be8c4a8e8b21ee3f19527f7e73
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-24 12:18:28 +0900

    add: お気に入り一覧のRowViewとお気に入り詳細viewを扱うファイルの作成

---
+++ QuickGourmet/Views/Search/ShopDetail/FavoriteShopDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteShopDetailView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/24.
+//
+
+import SwiftUI
+
+struct FavoriteShopDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteShopDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteShopDetailView()
+    }
+}
---
+++ QuickGourmet/Views/Search/ShopList/Favorite/FavoriteShopRowView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteShopRowView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/24.
+//
+
+import SwiftUI
+
+struct FavoriteShopRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteShopRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteShopRowView()
+    }
+}
