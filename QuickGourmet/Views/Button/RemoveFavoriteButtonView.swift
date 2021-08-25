1f209bcb62050b060bdb64cfe2ccc3825105b524
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-25 14:47:59 +0900

    feat: お気に入り解除用の詳細Viewを作成

--- QuickGourmet/Views/Button/RemoveFavoriteButtonView.swift
+++ QuickGourmet/Views/Button/RemoveFavoriteButtonView.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct RemoveFavoriteButtonView: View {
+    var onTapped: () -> Void
+    @State private var isRemoveFavorite = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            onTapped()
+            isRemoveFavorite.toggle()
+        }) {
+            Text(isRemoveFavorite ? "解除済み" : "解除")
+                .foregroundColor(Color.white)
+                .font(.body)
+                .padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .background(isRemoveFavorite ? Color.gray : Color.orange)
+                .cornerRadius(100)
+                .lineLimit(1)
+        }
+        .disabled(isRemoveFavorite)
     }
 }
 
 struct RemoveFavoriteButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        RemoveFavoriteButtonView()
+        RemoveFavoriteButtonView(onTapped: {})
     }
 }
