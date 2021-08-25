d9b532a22004302ef5324be8bd8593da3b05c9d6
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-25 11:25:42 +0900

    add: お気に入り削除ボタンとお気に入りリストから遷移した詳細Viewを管理するファイルを作成
    本当はお気に入りボタンを一つで管理したいがやり方がわからない。。お気に入り用ボタンと削除用ボタンを作成して登録解除を管理。ボタンのだしわけが必要なのでDetailInfoViewをふたつ作成。

---
+++ QuickGourmet/Views/Button/RemoveFavoriteButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  RemoveFavoriteButtonView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/25.
+//
+
+import SwiftUI
+
+struct RemoveFavoriteButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RemoveFavoriteButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        RemoveFavoriteButtonView()
+    }
+}
