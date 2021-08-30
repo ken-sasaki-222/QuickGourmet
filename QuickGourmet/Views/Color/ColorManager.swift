9456271e8cd5f235248c026ac54fcff5466e1d09
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-30 20:28:51 +0900

    feat: ColorManager更新

--- QuickGourmet/Views/Color/ColorManager.swift
+++ QuickGourmet/Views/Color/ColorManager.swift
@@ -10,4 +10,7 @@ import SwiftUI
 
 struct ColorManager {
     static let baseColor = Color("base_color")
+    static let mainColor = Color("main_color")
+    static let white = Color("white")
+    static let black = Color("black")
 }
