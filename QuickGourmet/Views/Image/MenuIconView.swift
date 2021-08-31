8d161a57d4c487c266576481e6be8cef30b50342
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-31 11:56:38 +0900

    add: メニューアイコンを扱うファイルを作成

---
+++ QuickGourmet/Views/Image/MenuIconView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuIconView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/08/31.
+//
+
+import SwiftUI
+
+struct MenuIconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuIconView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuIconView()
+    }
+}
