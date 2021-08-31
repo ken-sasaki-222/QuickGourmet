7df8a1b5687ed3dc89206c61d94510e506421a9c
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-31 11:55:34 +0900

    update: Colors.xcassets更新

--- QuickGourmet/Views/Color/ColorManager.swift
+++ QuickGourmet/Views/Color/ColorManager.swift
@@ -13,5 +13,7 @@ struct ColorManager {
     static let mainColor = Color("main_color")
     static let white = Color("white")
     static let black = Color("black")
+    static let black_white = Color("black_white")
     static let gray = Color("gray")
+    static let red = Color("red")
 }
