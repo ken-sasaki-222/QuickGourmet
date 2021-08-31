8c3f285d68f1afc3026f53581a3fd2f18af2d23a
佐々木　謙 <nkekisasa222@gmail.com>
2021-08-31 13:18:01 +0900

    update: ColorManager更新

--- QuickGourmet/Views/Color/ColorManager.swift
+++ QuickGourmet/Views/Color/ColorManager.swift
@@ -14,6 +14,7 @@ struct ColorManager {
     static let white = Color("white")
     static let black = Color("black")
     static let black_white = Color("black_white")
+    static let white_black = Color("white_black")
     static let gray = Color("gray")
     static let red = Color("red")
     static let icon_red = Color("icon_red")
