1c537d288cbb861c1b59c1204fc6af90f0dc9b6c
佐々木　謙 <nkekisasa222@gmail.com>
2021-09-04 13:50:43 +0900

    feat: ジャンルタイプを定義

--- QuickGourmet/Model/Shop/GenreType.swift
+++ QuickGourmet/Model/Shop/GenreType.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum GenreType: Int {
+    case izakaya = 0
+    case dainingubar = 1
+    case sousakuryouri = 2
+    case wasyoku = 3
+    case yousyoku = 4
+    case italian = 5
+    case tyuuka = 6
+    case yakiniku = 7
+    case aziaryouri = 8
+    case kakukokuryouri = 9
+    case karaoke = 10
+    case kakuteru = 11
+    case ramen = 12
+    case cafe = 13
+    case other = 14
+    case okonomiyaki = 15
+    case corearyouri = 16
+}
