97f538713133063b0ceacdd434af457ce3a33d87
佐々木　謙 <nkekisasa222@gmail.com>
2021-09-17 18:59:10 +0900

    add: 検索機能を扱うRepositoryファイルを作成

---
+++ QuickGourmet/Repository/Search/MockShopSearchRepository.swift
@@ -0,0 +1,8 @@
+//
+//  MockShopSearchRepository.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/09/17.
+//
+
+import Foundation
---
+++ QuickGourmet/Repository/Search/ShopSearchRepository.swift
@@ -0,0 +1,8 @@
+//
+//  ShopSearchRepository.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/09/17.
+//
+
+import Foundation
