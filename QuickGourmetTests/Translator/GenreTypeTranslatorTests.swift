34923cd75a7fac047bb407e0db01596f10c39821
佐々木　謙 <nkekisasa222@gmail.com>
2021-09-19 12:17:16 +0900

    add: GenreTypeTranslatorクラスをテストするファイルを作成

---
+++ QuickGourmetTests/Translator/GenreTypeTranslatorTests.swift
@@ -0,0 +1,23 @@
+//
+//  GenreTypeTranslatorTests.swift
+//  QuickGourmetTests
+//
+//  Created by sasaki.ken on 2021/09/19.
+//
+
+import XCTest
+
+class GenreTypeTranslatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testTranslate_expectAndResultEqual() throws {
+        
+    }
+}
