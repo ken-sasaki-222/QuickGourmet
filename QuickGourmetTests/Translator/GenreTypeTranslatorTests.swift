690086bc1e83656b747cdd2aec83808fca1e32c4
佐々木　謙 <nkekisasa222@gmail.com>
2021-09-19 12:28:56 +0900

    test: GenreTypeTranslatorクラスのテストを追加

--- QuickGourmetTests/Translator/GenreTypeTranslatorTests.swift
+++ QuickGourmetTests/Translator/GenreTypeTranslatorTests.swift
@@ -5,10 +5,10 @@
 //  Created by sasaki.ken on 2021/09/19.
 //
 
+@testable import QuickGourmet
 import XCTest
 
 class GenreTypeTranslatorTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,6 +18,46 @@ class GenreTypeTranslatorTests: XCTestCase {
     }
 
     func testTranslate_expectAndResultEqual() throws {
-        
+        let inputs: [GenreType] = [.izakaya,
+                                   .dainingubar,
+                                   .sousakuryouri,
+                                   .wasyoku,
+                                   .yousyoku,
+                                   .italian,
+                                   .tyuuka,
+                                   .yakiniku,
+                                   .aziaryouri,
+                                   .kakukokuryouri,
+                                   .karaoke,
+                                   .kakuteru,
+                                   .ramen,
+                                   .cafe,
+                                   .other,
+                                   .okonomiyaki,
+                                   .corearyouri]
+
+        let expects = ["G001",
+                       "G002",
+                       "G003",
+                       "G004",
+                       "G005",
+                       "G006",
+                       "G007",
+                       "G008",
+                       "G009",
+                       "G010",
+                       "G011",
+                       "G012",
+                       "G013",
+                       "G014",
+                       "G015",
+                       "G016",
+                       "G017"]
+
+        for (index, input) in inputs.enumerated() {
+            let expect = expects[index]
+            let result = GenreTypeTranslator.translate(genre: input)
+            XCTAssertEqual(result, expect)
+        }
     }
 }
