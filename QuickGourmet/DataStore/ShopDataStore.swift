f5f7f0bcd7e778d6a9ad691f8f0e480e2241b49a
佐々木　謙 <nkekisasa222@gmail.com>
2021-12-11 17:56:47 +0900

    fest: async/awaitでDataStore作成

--- QuickGourmet/DataStore/ShopDataStore.swift
+++ QuickGourmet/DataStore/ShopDataStore.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 class ShopDataStore {
-    
+    private let shared = URLSession.shared
+    private let decoder = JSONDecoder()
+
+    func fetchShopDate(requestString: String) async throws -> [Shop] {
+        guard let requestUrl = URL(string: requestString) else {
+            throw NSError(domain: "Request url notfound.", code: -1, userInfo: nil)
+        }
+        print("requestURL: \(requestUrl)")
+        let request = URLRequest(url: requestUrl)
+        let (date, response) = try await shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200
+        else {
+            throw NSError(domain: "HttpResponse error.", code: -2, userInfo: nil)
+        }
+
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let decodeDate = try decoder.decode(HotPepperResponse.self, from: date)
+        let shopes = decodeDate.results.shop
+        return shopes
+    }
 }
