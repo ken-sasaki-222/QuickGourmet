4ff0386c54143ab7d16d90374fb17572b0f3a9b5
佐々木　謙 <nkekisasa222@gmail.com>
2021-12-11 17:59:47 +0900

    test: 不要なテストなので削除

--- QuickGourmetTests/ViewModel/QuickSearchViewModelTests.swift
+++ QuickGourmetTests/ViewModel/QuickSearchViewModelTests.swift
@@ -20,77 +20,7 @@ class QuickSearchViewModelTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testGetShopData_requestKeyIsNil_expectAndResultEqual() throws {
-        let request = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=&lat=35.6465202848107&lng=139.62975824675942&range=3&genre=G001&count=100&format=json"
-        let expectation: XCTestExpectation = expectation(description: "wait for finish")
-        let expectMessage = "The data couldn’t be read because it is missing."
-
-        mockShopSearchRepository.fetchShopData(requestString: request) { result in
-            switch result {
-            case .success:
-                return
-            case let .failure(error):
-                let result = error.localizedDescription
-                XCTAssertEqual(expectMessage, result)
-            }
-            expectation.fulfill()
-        }
-        wait(for: [expectation], timeout: 10)
-    }
-
-    func testGetShopData_requestLocationIsNotJapan_expectAndResultEqual() throws {
-        let request = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=8038faee5cb2aff5&lat=37.785834&lng=-122.406417&range=3&genre=G001&count=100&format=json"
-        let expectation: XCTestExpectation = expectation(description: "wait for finish")
-
-        mockShopSearchRepository.fetchShopData(requestString: request) { result in
-            switch result {
-            case let .success(shopes):
-                let result = shopes
-                let expect = 0
-                XCTAssertEqual(expect, result.count)
-            case .failure:
-                return
-            }
-            expectation.fulfill()
-        }
-        wait(for: [expectation], timeout: 10)
-    }
-
-    func testGetShopData_requestRangeIsNil_resultIsNotNil() throws {
-        let request = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=8038faee5cb2aff5&lat=35.6465202848107&lng=139.62975824675942&range=&genre=&count=100&format=json"
-        let expectation: XCTestExpectation = expectation(description: "wait for finish")
-
-        mockShopSearchRepository.fetchShopData(requestString: request) { result in
-            switch result {
-            case let .success(shopes):
-                let result = shopes
-                XCTAssertNotNil(result)
-            case .failure:
-                return
-            }
-            expectation.fulfill()
-        }
-        wait(for: [expectation], timeout: 10)
-    }
-
-    func testGetShopData_requestGenreIsNil_resultIsNotNil() throws {
-        let request = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=8038faee5cb2aff5&lat=35.6465202848107&lng=139.62975824675942&range=3&genre=&count=100&format=json"
-        let expectation: XCTestExpectation = expectation(description: "wait for finish")
-
-        mockShopSearchRepository.fetchShopData(requestString: request) { result in
-            switch result {
-            case let .success(shopes):
-                let result = shopes
-                XCTAssertNotNil(result)
-            case .failure:
-                return
-            }
-            expectation.fulfill()
-        }
-        wait(for: [expectation], timeout: 10)
-    }
-
+    
     func testRecordSearchListLaunchCount_ValidateTrueOrFalse() throws {
         XCTContext.runActivity(named: "searchListLaunchCountが10の倍数の場合") { _ in
             userRepository.searchListLaunchCount = 8
