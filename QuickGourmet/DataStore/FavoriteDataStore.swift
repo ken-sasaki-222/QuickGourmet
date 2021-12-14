9745e874e78f2db575fcad781c9b450f36716abc
佐々木　謙 <nkekisasa222@gmail.com>
2021-12-14 11:22:51 +0900

    feat: 削除機能をDataStoreに書いた

--- QuickGourmet/DataStore/FavoriteDataStore.swift
+++ QuickGourmet/DataStore/FavoriteDataStore.swift
@@ -11,14 +11,11 @@ import Foundation
 class FavoriteDataStore {
     private let db = Firestore.firestore()
     private var favoriteShopes = [FavoriteShop]()
+    private let shopData = "shopData"
+    private let favorite = "favorite"
 
     func saveFavoriteShopData(favoriteShop: FavoriteShop, deviceId: String, onSuccess: @escaping () -> Void, onFailure: @escaping () -> Void) {
-        // test firebase用意 -> 記事にする
-        // save test書く
-        // get, delete書く
-        // get delete test書く
-
-        db.collection("shopData").document(deviceId).collection("favoriteShopes").addDocument(data: [
+        db.collection(shopData).document(deviceId).collection(favorite).addDocument(data: [
             "name": favoriteShop.name,
             "address": favoriteShop.address,
             "mobileAccess": favoriteShop.mobileAccess,
@@ -36,6 +33,21 @@ class FavoriteDataStore {
                 onFailure()
             } else {
                 print("Success adding document.")
+                onSuccess()
+            }
+        }
+    }
+
+    // get, delete書く
+    // get delete test書く
+
+    func deleteFavoriteShopData(documentId: String, deviceId: String, onSuccess: @escaping () -> Void, onFailure: @escaping () -> Void) {
+        db.collection(shopData).document(deviceId).collection(favorite).document(documentId).delete { error in
+            if let error = error {
+                print("Error removing document:", error)
+                onFailure()
+            } else {
+                print("Document successfully removed.")
                 onSuccess()
             }
         }
