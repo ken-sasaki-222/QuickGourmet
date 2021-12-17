76b497990ea9f5c3d271722012918efcc4e08003
佐々木　謙 <nkekisasa222@gmail.com>
2021-12-17 16:08:34 +0900

    add: LocationStatusTypeを追加

---
+++ QuickGourmet/Model/Type/LocationStatusType.swift
@@ -0,0 +1,18 @@
+//
+//  LocationStatusType.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/12/17.
+//
+
+import Foundation
+
+enum LocationStatusType {
+    case notDetermined
+    case restricted
+    case denied
+    case authorizedAlways
+    case authorizedWhenInUse
+    case authorized
+    case unknown
+}
