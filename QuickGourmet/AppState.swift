663809e4aae418df6666351698507d2c2f5eb13b
佐々木　謙 <nkekisasa222@gmail.com>
2021-12-22 21:08:14 +0900

    feat: rootViewを切り替えるシングルトンクラス作成

---
+++ QuickGourmet/AppState.swift
@@ -0,0 +1,27 @@
+//
+//  AppState.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/12/22.
+//
+
+import Foundation
+
+class AppState: ObservableObject {
+    @Published private(set) var rootView: RootViews = .home
+    static let shared = AppState()
+
+    private init() {
+        // no action..
+    }
+
+    enum RootViews {
+        case location
+        case tracking
+        case home
+    }
+
+    func changeRootView(rootView: RootViews) {
+        self.rootView = rootView
+    }
+}
