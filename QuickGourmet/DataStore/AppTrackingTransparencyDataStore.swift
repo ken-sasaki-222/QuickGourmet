8f733f41f68c5b7e793684b70e06c2e28093fbcd
佐々木　謙 <nkekisasa222@gmail.com>
2021-12-25 17:07:41 +0900

    feat: ATTのDataStore作成

--- QuickGourmet/DataStore/AppTrackingTransparencyDataStore.swift
+++ QuickGourmet/DataStore/AppTrackingTransparencyDataStore.swift
@@ -5,4 +5,17 @@
 //  Created by sasaki.ken on 2021/12/23.
 //
 
+import AppTrackingTransparency
 import Foundation
+
+class AppTrackingTransparencyDataStore {
+    func requestAuthorization(complication: @escaping () -> Void) {
+        ATTrackingManager.requestTrackingAuthorization { _ in
+            complication()
+        }
+    }
+
+    func getAuthorizationStatus() -> ATTrackingManager.AuthorizationStatus {
+        ATTrackingManager.trackingAuthorizationStatus
+    }
+}
