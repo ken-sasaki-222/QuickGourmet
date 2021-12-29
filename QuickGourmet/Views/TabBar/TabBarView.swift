dbdf885edeb166dab65e9bdb25d66464d4dfec55
佐々木　謙 <nkekisasa222@gmail.com>
2021-12-29 18:58:11 +0900

    update: Indicatorを追加

--- QuickGourmet/Views/TabBar/TabBarView.swift
+++ QuickGourmet/Views/TabBar/TabBarView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TabBarView: View {
     @State private var section = 0
+    @State private var isShowsIndicator = false
     private let locatePermissionVM = LocatePermissionViewModel()
     private let appTrackingTransparencyVM = AppTrackingTransparencyViewModel()
 
@@ -17,30 +18,48 @@ struct TabBarView: View {
     }
 
     var body: some View {
-        TabView(selection: $section) {
-            QuickSearchView()
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("ホーム")
+        ZStack {
+            TabView(selection: $section) {
+                QuickSearchView()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("ホーム")
+                    }
+                    .tag(0)
+                FavoriteListView()
+                    .tabItem {
+                        Image(systemName: "bookmark")
+                        Text("ブックマーク")
+                    }
+                    .tag(1)
+                MenuView()
+                    .tabItem {
+                        Image(systemName: "menucard")
+                        Text("メニュー")
+                    }
+                    .tag(2)
+            }
+            .accentColor(ColorManager.mainColor)
+            .onAppear {
+                isShowsIndicator = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                    setUpRootView()
                 }
-                .tag(0)
-            FavoriteListView()
-                .tabItem {
-                    Image(systemName: "bookmark")
-                    Text("ブックマーク")
+            }
+            if isShowsIndicator {
+                ZStack {
+                    ColorManager.black.opacity(0.5)
+                        .edgesIgnoringSafeArea(.all)
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 140, height: 120, alignment: .center)
+                        .foregroundColor(ColorManager.black.opacity(0.7))
+                    VStack {
+                        ActivityIndicator()
+                        Text("Loading...")
+                            .foregroundColor(ColorManager.white)
+                            .padding(.top, 3)
+                    }
                 }
-                .tag(1)
-            MenuView()
-                .tabItem {
-                    Image(systemName: "menucard")
-                    Text("メニュー")
-                }
-                .tag(2)
-        }
-        .accentColor(ColorManager.mainColor)
-        .onAppear {
-            DispatchQueue.main.async {
-                setUpRootView()
             }
         }
     }
@@ -54,10 +73,13 @@ struct TabBarView: View {
         let isTrackingPermissionViewEnabled = appTrackingTransparencyVM.openTrackingPermissionViewEnabled
 
         if isLocatePermissionViewEnabled {
+            isShowsIndicator = false
             RootViewHelper.shared.changeRootView(rootView: .location)
         } else if isLocatePermissionViewEnabled == false && isTrackingPermissionViewEnabled == true {
+            isShowsIndicator = false
             RootViewHelper.shared.changeRootView(rootView: .tracking)
         } else if isLocatePermissionViewEnabled == false && isTrackingPermissionViewEnabled == false {
+            isShowsIndicator = false
             RootViewHelper.shared.changeRootView(rootView: .home)
         }
     }
