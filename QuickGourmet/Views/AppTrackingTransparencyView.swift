fa0a8a310f120ccb2c3516f6c944d9b834e94c85
佐々木　謙 <nkekisasa222@gmail.com>
2021-12-29 17:14:24 +0900

    update: Traking許可専用画面のViewを作成

--- QuickGourmet/Views/AppTrackingTransparencyView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AppTrackingTransparencyView.swift
-//  QuickGourmet
-//
-//  Created by sasaki.ken on 2021/12/23.
-//
-
-import Foundation
---
+++ QuickGourmet/Views/Location/AppTrackingTransparencyView.swift
@@ -0,0 +1,46 @@
+//
+//  AppTrackingTransparencyView.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/12/25.
+//
+
+import SwiftUI
+
+struct AppTrackingTransparencyView: View {
+    @ObservedObject var appTrackingTransparencyViewModel = AppTrackingTransparencyViewModel()
+
+    var body: some View {
+        ZStack {
+            Image("food_tracking")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            ColorManager.black.opacity(0.3)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Text("広告トラッキングを許可してください")
+                    .font(.custom(FontManager.Mplus.medium, size: 22))
+                    .foregroundColor(ColorManager.white)
+                    .lineLimit(1)
+                    .padding(.bottom, 3)
+                Text("次の画面で『許可』を選択することで、\nあなたに関係のない広告が表示されなくなります")
+                    .font(.custom(FontManager.Mplus.medium, size: 16))
+                    .foregroundColor(ColorManager.white)
+                    .lineLimit(2)
+                    .padding(.bottom, 30)
+                PermissionButtonView {
+                    // ここでステータス取得？
+                }
+                .padding(.bottom, 50)
+            }
+        }
+    }
+}
+
+struct AppTrackingTransparencyView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppTrackingTransparencyView()
+    }
+}
