bc3e6b2a8a5fc488c244d7c2cd56ce571bebd459
佐々木　謙 <nkekisasa222@gmail.com>
2021-12-30 15:01:59 +0900

    add: Review機能を集約するRepositoryを追加

---
+++ QuickGourmet/Repository/ReviewRepository.swift
@@ -0,0 +1,12 @@
+//
+//  ReviewRepository.swift
+//  QuickGourmet
+//
+//  Created by sasaki.ken on 2021/12/30.
+//
+
+import Foundation
+
+class ReviewRepository: ReviewRepositoryInterface {
+    
+}
